1351512013efc5009a2eac4412cd0d5c08418432
tradingwins1 <sritradingwins@gmail.com>
2025-03-31 01:08:25 +0000

    .replit and replit.nix changes are being committed

--- replit.nix
+++ replit.nix
@@ -1,15 +1,8 @@
-{pkgs}: {
+{ pkgs }: {
   deps = [
-    pkgs.glibcLocales
-    pkgs.tk
-    pkgs.tcl
-    pkgs.qhull
-    pkgs.pkg-config
-    pkgs.gtk3
-    pkgs.gobject-introspection
-    pkgs.ghostscript
-    pkgs.freetype
-    pkgs.ffmpeg-full
-    pkgs.cairo
+    pkgs.python311Full
+    pkgs.git
+    pkgs.cacert
   ];
 }
+
