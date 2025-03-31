11a9fb8a61c9b9bb8f2a92aaa8b0560377353baa
tradingwins1 <sritradingwins@gmail.com>
2025-03-31 03:18:56 +0000

    Few more changes are being committed

--- replit.nix
+++ replit.nix
@@ -1,13 +1,11 @@
 { pkgs }: {
   deps = [
     pkgs.python311Full
-    pkgs.python311Packages.schedule
-    pkgs.python311Packages.pytz
-    pkgs.python311Packages.python-dotenv
-    pkgs.python311Packages.pandas
-    pkgs.python311Packages.yfinance
-    pkgs.python311Packages.requests
+    pkgs.glibcLocales
   ];
 }
+
+
+
 
 
